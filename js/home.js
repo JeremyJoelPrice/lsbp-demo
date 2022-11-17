107d037b274883a0c284facf390190772324ffb6
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2022-11-17 17:28:12 +0000

    add events section to home page

--- js/home.js
+++ js/home.js
@@ -1,11 +1,29 @@
-import { displayHtml, getFirstElementOfClass, loadSnippet } from "./ajax-utility.js";
+import {
+	displayHtml,
+	getElementsOfClass,
+	getFirstElementOfClass,
+	loadSnippet
+} from "./ajax-utility.js";
 
 export const homeScript = () => {
 	assignListeners();
 };
 
 function assignListeners() {
-	getFirstElementOfClass("diploma-training-card__label").addEventListener("click", () => {
-		loadSnippet("training");
+	getFirstElementOfClass("diploma-training-card__label").addEventListener(
+		"click",
+		() => {
+			loadSnippet("training");
+		}
+	);
+	getElementsOfClass("event-banner__button").forEach((button) => {
+		button.addEventListener("click", () => {
+			loadSnippet("training");
+		});
 	});
+	document
+		.getElementById("events-link-to-clinic")
+		.addEventListener("click", () => {
+			loadSnippet("student clinic");
+		});
 }
