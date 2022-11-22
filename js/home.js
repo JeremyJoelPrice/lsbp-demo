8aeea4e18fc71d60d46701bd9a12aa1dcc2cf91d
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2022-11-22 21:57:30 +0000

    make all homepage links show an alert

--- js/home.js
+++ js/home.js
@@ -1,29 +1,13 @@
-import {
-	displayHtml,
-	getElementsOfClass,
-	getFirstElementOfClass,
-	loadSnippet
-} from "./ajax-utility.js";
+import { getElementsOfClass } from "./ajax-utility.js";
 
 export const homeScript = () => {
 	assignListeners();
 };
 
 function assignListeners() {
-	getFirstElementOfClass("diploma-training-card__label").addEventListener(
-		"click",
-		() => {
-			loadSnippet("training");
-		}
-	);
-	getElementsOfClass("event-banner__button").forEach((button) => {
-		button.addEventListener("click", () => {
-			loadSnippet("training");
+	getElementsOfClass("link").forEach((link) => {
+		link.addEventListener("click", () => {
+			alert("if you hire Jeremy, these links will work");
 		});
 	});
-	document
-		.getElementById("events-link-to-clinic")
-		.addEventListener("click", () => {
-			loadSnippet("student clinic");
-		});
 }
